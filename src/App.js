ab50ecae5ae6ca2a417e036711eccc1eabedf0d1
PRAJWAL KARALE <prajwalkarale@PRAJWALs-MacBook-Pro.local>
2023-07-30 18:53:56 -0700

    Implementing useState in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 function App () {
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+        setCount(count + 1);
+    } 
     return <div>
-
+        <button onClick={handleClick}>Add Animal</button>
+        <div>
+            Number of Animals: {count}
+        </div>
     </div>
 }
 
