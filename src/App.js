129461dd29dc0d6e3c3ea815a3212858503c0ee0
PRAJWAL KARALE <prajwalkarale@PRAJWALs-MacBook-Pro.local>
2023-08-01 19:38:10 -0700

    Added css files and also images to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import './App.css';
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal () {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    const index = Math.floor(Math.random() * animals.length);
+    return animals[index];
+}
 function App () {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
     const handleClick = () => {
-        setCount(count + 1);
-    } 
-    return <div>
-        <button onClick={handleClick}>Add Animal</button>
-        <div>
-            Number of Animals: {count}
-        </div>
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+    
+    const renderedAnimals = animals.map((animal, index) => {
+        return<AnimalShow type = { animal } key={ index } />
+    });
+    return <div className='app'>
+        <button className='button-73' onClick={handleClick}>Add Animal</button>
+        <div className='animalList'>{renderedAnimals}</div>
     </div>
 }
 
